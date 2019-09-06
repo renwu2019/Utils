bff07cfa64a2ce246dc091a70a2a0678fc29676b
renwu2019 <47552824+renwu2019@users.noreply.github.com>
2019-09-06 22:01:45 +0800

    Update TimeUtils.java
    修改方法名称，更易理解

--- TimeUtils.java
+++ TimeUtils.java
@@ -12,7 +12,7 @@ public class TimeUtil {
     private static final String DATE_FORMAT_1 = "yyyy年MM月dd日";
     private static final String DATE_FORMAT_2 = "yyyy年MM月dd日HH : mm";
 
-    public static long getTimestamp1(String dateString) { //将字符串类型转成long类型  年月日
+    public static long date2long(String dateString) { //将字符串类型转成long类型  年月日
         try {
             //月份那用了中文，这里边便改成简体中文,原：Locale.ENGLISH
             Date date = new SimpleDateFormat(DATE_FORMAT_1, Locale.getDefault()).parse(dateString);
@@ -22,7 +22,7 @@ public class TimeUtil {
         }
         return 0;
     }
-    public static long getTimestamp2(String dateString) { //将字符串类型转成long类型  年月日时分
+    public static long dateTime2long(String dateString) { //将字符串类型转成long类型  年月日时分
         try {
             //月份那用了中文，这里边便改成简体中文,原：Locale.ENGLISH
             Date date = new SimpleDateFormat(DATE_FORMAT_2, Locale.getDefault()).parse(dateString);
@@ -33,7 +33,7 @@ public class TimeUtil {
         return 0;
     }
 
-    public static String getDateString(long time) { //将long类型格式化成字符类型  年月日时分
+    public static String long2dateTime(long time) { //将long类型格式化成字符类型  年月日时分
         Date date = new Date(time);
         DateFormat format = new SimpleDateFormat(DATE_FORMAT_2, Locale.getDefault());
         format.setTimeZone(TimeZone.getDefault());
